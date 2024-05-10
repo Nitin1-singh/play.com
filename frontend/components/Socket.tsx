809c71dc15ca2ca3f0bf77610c9277b243e27cb6
Nitin1-singh <nitinforcoding@gmail.com>
2024-05-10 09:11:27 +0530

    update-script-nodejs

--- frontend/components/Socket.tsx
+++ frontend/components/Socket.tsx
@@ -3,11 +3,10 @@ import { useEffect, useState } from "react";
 import { Socket, io } from "socket.io-client";
 
 export function useSocket() {
-  const API = "http://localhost:8000"
   const [socket, setSocket] = useState<Socket | null>(null)
   useEffect(() => {
     if (socket != null) return
-    const newSocket = io(API)
+    const newSocket = io(process.env.NEXT_PUBLIC_API as string,{})
     setSocket(newSocket)
   }, [])
   return { socket }
