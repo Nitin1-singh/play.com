166e10723e32391de06f5bdbe07a34a12869e5ac
Nitin1-singh <nitinforcoding@gmail.com>
2024-05-11 08:19:35 +0530

    added loki-grafana-promethus

--- server-nodejs/src/index.ts
+++ server-nodejs/src/index.ts
@@ -2,7 +2,9 @@ import express from "express"
 import { createServer } from "http"
 import { Server } from "socket.io"
 import { GameManager } from "./GameManager"
-
+import { register, collectDefaultMetrics } from "prom-client"
+import { createLogger, transports } from "winston"
+import LokiTransport from "winston-loki"
 // env variable
 const PORT = 8000
 export const INIT_GAME = "init_game"
@@ -11,6 +13,12 @@ export const GAME_OVER = "game_over"
 export const color = "color"
 export const TURN = "turn"
 export const UPDATE_BOARD = "board"
+
+
+// metrics for monitoring
+const collectDefaultMetric = collectDefaultMetrics
+collectDefaultMetric({ register: register })
+
 // variable
 const app = express()
 const server = createServer(app)
@@ -18,7 +26,25 @@ const io = new Server(server, { cors: { origin: "*" } })
 
 const gameManager = new GameManager()
 
-app.get("/", (req, res) => res.json("Health Check"))
+// for logging
+const options = {
+  transports: [
+    new LokiTransport({host:"http://localhost:3100"})
+  ]
+}
+const logger = createLogger(options)
+
+// for metrics
+app.get("/metrics", async (req, res) => {
+  logger.info("Req came on /metrics route")
+  logger.error("Custom error")
+  
+  res.setHeader("Content-Type", register.contentType)
+  const metrics = await register.metrics()
+  res.send(metrics)
+})
+
+
 
 io.on("connection", (socket) => {
   console.log("socket connect", socket.id)
